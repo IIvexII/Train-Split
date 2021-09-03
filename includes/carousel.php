d277b1fa63e139849cfc9d41774b4ba2a94d3b30
Zafeer Hafeez <mrh4ck3r1738@gmail.com>
2021-09-03 16:13:50 +0500

    First Commit: Basic template added.

---
+++ includes/carousel.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <h1 class="text-uppercase text-center">
+                Get Your seat Book
+                <span class="text-danger d-block">Now!</span>
+            </h1>
+            <form action="" method="post">
+                <div class="container mt-5">
+                    <div class="row">
+                        <div class="col ms-1">
+                            <input type="text" class="form-control" placeholder="From">
+                        </div>
+                        <div class="col">
+                            <input type="text" class="form-control" placeholder="To">
+                        </div>
+                        <div class="col">
+                            <input type="submit" value="Book Now" class="btn btn-danger">
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-5"><img id="head-train" width="400" src="images/train.png" alt=""></div>
+    </div>
+</div>
